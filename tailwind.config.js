f8b1dc64d5798f53133fe5a58d79cebd26b6424e
Nils Diekmann <github@kinneko.de>
2025-08-30 09:39:12 +0200

    - remove safelist content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,6 @@ module.exports = {
     './src/_includes/**/*.{njk,md,html}'
   ],
   safelist: [
-    'hidden',
-    'md:hidden',
-    'md:flex',
-    'md:block',
-    'flex',
-    'flex-col',
-    'space-x-6',
-    'space-y-2',
-    'items-center',
-    'justify-between',
-    'max-w-2xl',
-    'mx-auto',
-    'text-center',
-    'block',
-    'hover:text-blue-400'
   ],
   theme: {
     extend: {},
