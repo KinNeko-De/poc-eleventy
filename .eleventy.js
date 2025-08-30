aa5b0f096a83b403d695245f2d0a1c927827a19b
Nils Diekmann <github@kinneko.de>
2025-08-30 09:12:36 +0200

    - fix tailwind - fix icon

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+  // Add passthrough copy for CSS files and favicon
+  eleventyConfig.addPassthroughCopy("src/tailwind.css");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  
   eleventyConfig.addCollection("blog", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/blog/*.md").sort((a, b) => b.date - a.date);
   });
