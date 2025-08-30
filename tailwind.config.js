ff02f90d0a328a84167a41243d1da565d1cc347d
Nils Diekmann <github@kinneko.de>
2025-08-30 10:01:38 +0200

    - add themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     './src/_includes/**/*.{njk,md,html}'
   ],
   theme: {
-    extend: {},
+    // Define responsive breakpoints only
+    screens: {
+      sm: '640px', // small screens
+      md: '768px', // medium screens
+      lg: '1024px', // large screens
+      xl: '1280px', // extra large screens
+    }
   },
   plugins: [],
 };
