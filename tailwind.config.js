1aed5fb79ee85c5aed3654de6869d418872a7843
Nils Diekmann <github@kinneko.de>
2025-08-30 09:25:26 +0200

    - fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   content: [
-    './src/**/*.njk',
-    './src/**/*.md',
-    './src/**/*.html'
+    './src/**/*.{njk,md,html,js}',
+    './src/_includes/**/*.{njk,md,html}'
+  ],
+  safelist: [
+    'hidden',
+    'md:hidden',
+    'md:flex',
+    'md:block',
+    'flex',
+    'flex-col',
+    'space-x-6',
+    'space-y-2',
+    'items-center',
+    'justify-between',
+    'max-w-2xl',
+    'mx-auto',
+    'text-center',
+    'block',
+    'hover:text-blue-400'
   ],
   theme: {
     extend: {},
