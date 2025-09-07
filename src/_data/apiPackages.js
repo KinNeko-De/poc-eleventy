1ca1ddbd708e6bb66e9b85ac90183c2d3e7cd858
Nils Diekmann <github@kinneko.de>
2025-09-07 19:37:36 +0200

    - scalara types

--- src/_data/apiPackages.js
+++ src/_data/apiPackages.js
@@ -1,12 +1,17 @@
 
+
 export default async function () {
   const api = await import('./api.json', { assert: { type: 'json' } });
   const files = api.default.files;
   const packages = Array.from(new Set(files.map(f => f.package)));
+  const scalarTypes = api.default.scalarValueTypes || [];
+  const scalarTypeMap = Object.fromEntries(scalarTypes.map(t => [t.protoType, t]));
   return {
     packages,
     filesByPackage: Object.fromEntries(
       packages.map(pkg => [pkg, files.filter(f => f.package === pkg)])
-    )
+    ),
+    scalarTypes,
+    scalarTypeMap
   };
 }
